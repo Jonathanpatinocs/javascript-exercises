faf46a6445766ac0e63366bcbef0b5a44ad90896
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-04-19 20:46:37 -0400

    solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string = "string") {
+    let x = [];
+    let rev = "";
+    for ( let i = 0; i < string.length ; i++) {
+        x.push(string.charAt(i));
+    }
+    
+    while ( x.length > 0 ) {
+        rev += x.pop();
+    }
+    return rev;
 };
 
 // Do not edit below this line
