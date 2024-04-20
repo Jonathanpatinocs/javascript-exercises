66bc36af4e19cecc17f912e6a7c52e5d5605d732
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-04-19 21:14:59 -0400

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array = [], num = undefined, num2 = undefined, num3 = undefined, num4 = undefined) {
+    for (let i = 0; i < array.length ; i++) {
+        if (array[i] === num || array[i] === num2 || array[i] === num3 || array[i] === num4) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
