be6f82ac9a5da9a536d05bdadaa7a5c1418884bc
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-04-19 21:30:54 -0400

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(num = 0) {
+    if (num%4 === 0 && num%100 !== 0 || num%400 === 0 ) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
